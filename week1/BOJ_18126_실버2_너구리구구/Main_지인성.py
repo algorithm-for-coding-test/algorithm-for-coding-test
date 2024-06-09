2ad5e5c94fc04bacee716b1ce21f3ab822ffdf72
JIINSUNG <crm03008@gmail.com>
2024-06-09 12:49:52 +0900

    git commit --date "2 day ago" -m "boj18126_jiinsung"

---
+++ week1/BOJ_18126_실버2_너구리구구/Main_지인성.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+n = int(input())
+
+# 비용 정보
+graph = [[] for _ in range(n + 1)]
+
+visited = [False for _ in range(n+1)]
+
+dist = [0] * (n+1)
+
+for _ in range(n-1):
+    u, v, c = map(int, input().split())
+    graph[u].append([v, c])
+    graph[v].append([u, c])
+
+
+
+# start부터 goal 까지의 거리 중 가장 높은 answer가 답
+def bfs(start):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        # 큐에서 정점을 꺼냄
+        qv = queue.popleft()
+        # 해당 정점과 인접한 모든 정점을 방문처리하며 비용 계산
+        for iv, cost in graph[qv]:
+            if not visited[iv]:
+                visited[iv] = True
+                queue.append(iv)
+                dist[iv] = max(dist[iv], dist[qv] + cost)
+
+
+bfs(1)
+
+print(max(dist))
