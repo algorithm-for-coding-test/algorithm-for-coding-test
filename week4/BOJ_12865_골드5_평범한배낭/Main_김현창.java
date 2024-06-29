9e6ccad9d2e0423d71338401c8a6e67f4688ab11
h-spear <80024307+h-spear@users.noreply.github.com>
2024-06-29 21:22:24 +0900

    평범한 배낭 - 재귀

--- week4/BOJ_12865_골드5_평범한배낭/Main_김현창.java
+++ week4/BOJ_12865_골드5_평범한배낭/Main_김현창.java
@@ -33,3 +33,46 @@ public class Main_김현창 {
 	    br.close();
 	}
 }
+
+class AnotherSolution {
+
+	private static int N, K;
+	private static int[] weight;
+	private static int[] value;
+	private static int[][] dp;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		weight = new int[N + 1];
+		value = new int[N + 1];
+		for (int i = 1; i <= N; ++i) {
+			st = new StringTokenizer(br.readLine());
+			weight[i] = Integer.parseInt(st.nextToken());
+			value[i] = Integer.parseInt(st.nextToken());
+		}
+		dp = new int[N + 1][K + 1];
+		for (int i = 0; i <= N; ++i)
+			Arrays.fill(dp[i], -1);
+
+		System.out.println(knapsack(N, K));
+		br.close();
+	}
+
+	private static int knapsack(int n, int k) {
+		if (n == 0 || k <= 0)
+			return 0;
+		if (dp[n][k] != -1)
+			return dp[n][k];
+		if (weight[n] > k)
+			return dp[n][k] = knapsack(n - 1, k);
+		return dp[n][k] = Math.max(
+			knapsack(n - 1, k - weight[n]) + value[n],
+			knapsack(n - 1, k)
+		);
+	}
+}
