34b0c2056665c9dc484ff8cb762f7ce992f34d33
h-spear <80024307+h-spear@users.noreply.github.com>
2024-08-11 23:26:05 +0900

    reformat code

--- week7/BOJ_1946_실버1_신입사원/Main_지인성.java
+++ week7/BOJ_1946_실버1_신입사원/Main_지인성.java
@@ -1,11 +1,12 @@
+package week7.BOJ_1946_실버1_신입사원;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_지인성 {
 public static void main(String[] args) throws IOException {
   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   StringTokenizer st;
