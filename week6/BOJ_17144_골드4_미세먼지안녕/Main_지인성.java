d8df0e3d44f1d27891fcd4a120f7b8cb5564a9fe
h-spear <80024307+h-spear@users.noreply.github.com>
2024-08-25 12:40:07 +0900

    reformat code

--- week6/BOJ_17144_골드4_미세먼지안녕/Main_지인성.java
+++ week6/BOJ_17144_골드4_미세먼지안녕/Main_지인성.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_지인성 {
